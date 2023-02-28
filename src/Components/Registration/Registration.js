babe99c6a9ad51cae3991889be1e77085e2bacf6
tabrezsaqib <mohamedsaqib1996@gmail.com>
2023-02-28 11:02:59 -0400

    git push

--- src/Components/Registration/Registration.js
+++ src/Components/Registration/Registration.js
@@ -1,5 +1,7 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import './Registration.css';
+import Google from './Assets/Icons/google.svg';
+import Facebook from './Assets/Icons/facebook.svg';
 
 function Registration() {
 
@@ -8,32 +10,46 @@ function Registration() {
         email: "",
         password: "",
       });
-      const [btnState, setBtnState] = useState(false);
-      let nameError = '';
+    const [btnState, setBtnState] = useState(true);
+    let details = {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    };
+    let nameError = '';
     let emailError = '';
     let passwordError = '';
+    useEffect(() => {
+      fetch("http://localhost:8000/userDetails", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(details),
+        })
+          .then((resp) => {
+            console.log(resp.status);
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+    }, [user]);
       function handleUser(e){
-
-        if (e.target.name === "name") {
+        if(e.target.name === "name")
+        {
             setUser(() => ({ ...user, name: e.target.value }));
-          }
-      
-        if (e.target.name === "email") {
-            setUser(() => ({ ...user, email: e.target.value }));
-          }
-        if (e.target.name === "password") {
-            setUser(() => ({ ...user, password: e.target.value }));
-          }
-   
-        if (user.name !== "" && user.email !== "" && user.pasword !== ""){
-            setBtnState(true);
         }
-        else{
-            setBtnState(false);
+        if(e.target.name==="email")
+        {
+            setUser(() => ({ ...user, email: e.target.value }));
+        }
+        if(e.target.name === "password")
+        {
+            setUser(() => ({ ...user, password: e.target.value }));
         }
        }
-    function showPassword(e){
-        e.preventDefault();
+    function showPassword(p){
+        p.preventDefault();
         let x = user.password;
         if (x.type === "password") {
             x.type = "text";
@@ -42,32 +58,55 @@ function Registration() {
             x.type = "password";
         }
     }
-    function Validation(e){
-        if (e.target.name === "name" && e.target.value === "") {
-            nameError = "Please enter the name";
-            return false;
-          } else {
-            nameError = "";
+    useEffect(() => {
+      if (user.name !== "" && user.email !== "" && user.password !== ""){
+          setBtnState(false);
+      }
+      else{
+          setBtnState(true);
+      }
+  }, [user.name, user.email, user.password])
+    function SubmitData(err)
+    {
+      err.preventDefault();
+
+      if(user.name.length > 0 && user.email.length > 0 && user.password.length > 0){
+
+            fetch("http://localhost:8000/userDetails",{
+              method: "POST",
+              headers:{
+                'Content-Type': "apllication/json",
+              },
+              body: JSON.stringify(details),
+            }
+            ).then((resp) => {
+              console.log(resp.status);
+            } ).catch((error) => {
+              console.log(error.message);
+            });
+            } 
+      else {
+            alert("Please fill the form");
           }
-      
-          if (e.target.name === "email" && (e.target.value === "" || !e.target.value.endsWith("@gmail.com"))) {
-            emailError =  "Please enter a valid email";
-            return false;
-          } else {
-            emailError = "";
-          }
-      
-          if (e.target.name === "password" && (e.target.value === "" || e.target.value.length < 8)) {
-            passwordError = "Please enter a valid password";
-            return false;
-          } else {
-            passwordError = "";
-          }
+
+        if (user.name === "") {
+          nameError = "Please enter the first name";
+          return false;
+        } 
+        if (user.email === "" || !user.email.endsWith("@gmail.com")) {
+          emailError = "Please enter the valid Email";
+          return false;
+        } 
+        if (user.password === "" || user.password.length < 8) {
+          passwordError = "Please enter the valid password";
+          return false;
+        }
     }
+
   return (
     <div> 
             <div className='regmain'>
-                <form className='regform' onsubmit={Validation} method="get" action="">
+                <form className='regform' onsubmit={SubmitData} method="get" action="">
                     <div className='regtitle'>
                         <p className='regheader'>Register</p>
                         <p className="regnew">Already a member? <a href="#">Login</a></p>
@@ -109,10 +148,10 @@ function Registration() {
                     <p id="regoption">or register with</p>
                     <div className="regparent">
                         <div className="reggoogle">
-                            <a href="https://www.google.com/" id="reggo"><img src='./Assets/Icons/google.svg' alt="" className="regimg-google"/>Google</a>
+                            <a href="https://www.google.com/" id="reggo"><img src={Google} alt="" className="regimg-google"/>Google</a>
                         </div>
                         <div class="regfacebook">
-                            <a href="https://www.facebook.com/" id="regfb"><img src="./Assets/Icons/facebook.svg" alt="" className="regimg-google"/>Facebook</a>
+                            <a href="https://www.facebook.com/" id="regfb"><img src={Facebook} alt="" className="regimg-google"/>Facebook</a>
                         </div>
                     </div>
                 </form>
